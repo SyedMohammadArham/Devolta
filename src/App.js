5e4170baa25171388c7d30fd3605056e8b3cdef9
syed arham <syedmohammadarham73@gmail.com>
2026-04-01 03:09:44 +0500

    Updated portfolio and styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Navbar from './Components/Navbar';
 import About from './Components/About';
 import { Portfolio } from './Components/Portfolio';
 import Footer from './Components/Footer';
-
+import Softwaredevelopment from './Components/Services/Softwaredevelopment';
+import  Creativedesign  from './Components/Services/Creativedesign';
+import Marketing from './Components/Services/Marketing';
+import Ecommerce from './Components/Services/Ecommerce';
+import Contact from './Components/Contactus/ContactUs';
 function App() {
   return (
     <BrowserRouter>
@@ -17,7 +21,12 @@ function App() {
         <Route path="/services" element={<Services />} />
         <Route path="/about" element={<About />} />
         <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/softwaredevelopment" element={<Softwaredevelopment/>} />
+        <Route path="/creativedesign" element={<Creativedesign/>} />
+        <Route path="/marketing" element={<Marketing/>}/>
+        <Route path="/ecommerce" element={<Ecommerce/>} />
       </Routes>
+      <Contact/>
       <Footer />
     </BrowserRouter>
   );
